396bcc84515959131441a2aef2c04b91c90088d7
cferdinandi <chris@gomakethings.com>
2019-06-07 14:50:04 -0400

    New font loading strategy

--- themes/vanillajs/src/js/detects/zzz_loadFonts.js
+++ themes/vanillajs/src/js/detects/zzz_loadFonts.js
@@ -2,17 +2,14 @@
  * Load custom typeface
  */
 ;(function () {
-	var css = 'https://fonts.googleapis.com/css?family=PT+Serif:400,400i,700,700i';
+	if (!('fonts' in document)) return;
 	if (getCookie('fontsLoaded')) {
-		loadCSS(css);
 		document.documentElement.className += ' fonts-loaded';
-	} else {
-		loadCSS(css);
-		var font = new FontFaceObserver('PT Serif');
-		font.load().then(function () {
-			var expires = new Date(+new Date() + (7 * 24 * 60 * 60 * 1000)).toUTCString();
-			document.cookie = 'fontsLoaded=true; expires=' + expires;
-			document.documentElement.className += ' fonts-loaded';
-		});
+		return;
 	}
+	document.fonts.load('1em PT Serif').then(function () {
+		var expires = new Date(+new Date() + (7 * 24 * 60 * 60 * 1000)).toUTCString();
+		document.cookie = 'fontsLoaded=true; expires=' + expires;
+		document.documentElement.className += ' fonts-loaded';
+	});
 })();
