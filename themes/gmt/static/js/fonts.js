a44b89c5389dc60f511f77d4d0024526b99de16c
cferdinandi <chris@gomakethings.com>
2020-09-17 10:15:53 -0400

    Updated JS

--- themes/gmt/static/js/fonts.js
+++ themes/gmt/static/js/fonts.js
@@ -5,9 +5,11 @@
 	// Detect when font files are fully loaded before using them in the UI
 	if ('fonts' in document) {
 		Promise.all([
-			document.fonts.load('1em PT Serif'),
+			document.fonts.load('1em PT Sans'),
+			document.fonts.load('700 1em PT Sans'),
+			document.fonts.load('italic 1em PT Sans'),
+			document.fonts.load('italic 700 1em PT Sans'),
 			document.fonts.load('700 1em PT Serif'),
-			document.fonts.load('italic 1em PT Serif'),
 			document.fonts.load('italic 700 1em PT Serif')
 		]).then(function () {
 			document.documentElement.className += ' fonts-loaded';
