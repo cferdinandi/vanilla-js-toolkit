d33139ef163bca7b898d5d385102fbb175246a38
cferdinandi <chris@gomakethings.com>
2021-01-12 22:46:41 -0500

    Updated JS

--- themes/gmt/static/js/main.js
+++ themes/gmt/static/js/main.js
@@ -39,7 +39,6 @@
 	 */
 	var mailchimp = function (callback) {
 
-
 		//
 		// Variables
 		//
@@ -56,7 +55,8 @@
 		var messages = {
 			empty: 'Please provide an email address.',
 			notEmail: 'Please use a valid email address.',
-			success: 'Success! Thanks for inviting me to your inbox.'
+			success: 'Success! Thanks for inviting me to your inbox.',
+			failed: 'Something went wrong. Please try again.'
 		};
 
 		// Endpoint
@@ -68,49 +68,24 @@
 		//
 
 		/**
-		 * Serialize the form data into a query string
-		 * https://stackoverflow.com/a/30153391/1293256
-		 * @param  {Node}   form The form to serialize
-		 * @return {String}      The serialized form data
+		 * Serialize form data into a query string
+		 * @param  {Form}   form The form
+		 * @return {String}      The query string
 		 */
-		var serialize = function (form) {
-
-			// Setup our serialized data
-			var serialized = [];
-
-			// Loop through each field in the form
-			for (var i = 0; i < form.elements.length; i++) {
-
-				var field = form.elements[i];
-
-				// Don't serialize fields without a name, submits, buttons, file and reset inputs, and disabled fields
-				if (!field.name || field.disabled || field.type === 'file' || field.type === 'reset' || field.type === 'submit' || field.type === 'button') continue;
-
-				// Convert field data to a query string
-				if ((field.type !== 'checkbox' && field.type !== 'radio') || field.checked) {
-					serialized.push(encodeURIComponent(field.name) + "=" + encodeURIComponent(field.value));
-				}
+		var serializeForm = function (form) {
+			var arr = [];
+			var formData = new FormData(form);
+			for (var key of formData.keys()) {
+				arr.push(encodeURIComponent(key) + '=' + encodeURIComponent(formData.get(key)));
 			}
-
-			return serialized.join('&');
-
+			return arr.join('&');
 		};
 
-		var addRole = function () {
-			if (!status) return;
-			status.setAttribute('role', 'status');
-		};
-
-		var clearStatus = function () {
-
-			// Bail if there's no status container
-			if (!status) return;
-
-			// Wipe classes and update status HTML
-			showStatus(btn.getAttribute('data-processing'), true);
-
-		};
-
+		/**
+		 * Show a status message
+		 * @param  {String}  msg     The message to show
+		 * @param  {Boolean} success If true, the status was successful
+		 */
 		var showStatus = function (msg, success) {
 
 			// Bail if there's no status container
@@ -119,71 +94,68 @@
 			// Update the status message
 			status.textContent = msg;
 
-			// Set status class
-			if (success) {
-				status.className = 'success-message';
-				email.className = '';
-			} else {
-				status.className = 'error-message';
-				email.className = 'error';
-			}
+			// Set status classes
+			status.className = success ? 'success-message' : 'error-message';
+			email.className = success ? '' : 'error';
 
 		};
 
+		/**
+		 * Send data to the API
+		 * @param  {String} params The form parameters
+		 */
 		var sendData = function (params) {
-
-			// Set up our HTTP request
-			var xhr = new XMLHttpRequest();
-
-			// Setup our listener to process compeleted requests
-			xhr.onreadystatechange = function () {
-
-				// Only run if the request is complete
-				if ( xhr.readyState !== 4 ) return;
-
-				// Show status message
-				var success = xhr.status === 200 ? true : false;
-				var response = JSON.parse(xhr.responseText);
-				if (success) {
-					showStatus(messages.success, success);
-				} else {
-					showStatus(response.message, success);
+			fetch(endpoint, {
+				method: 'POST',
+				body: params,
+				headers: {
+					'Content-type': 'application/x-www-form-urlencoded'
 				}
+			}).then(function (response) {
+				return response.json();
+			}).then(function (data) {
 
-				// Reenable button
-				form.removeAttribute('data-submitting');
-
+				// Show status
+				var success = data.code >= 200 && response.code < 300 ? true : false;
+				showStatus(success ? messages.success : data.message, success);
 
 				// If there's a callback, run it
 				if (callback && typeof callback === 'function') {
-					callback(response);
+					callback(data);
 				}
 
-			};
-
-			// Create and send a GET request
-			// The first argument is the post type (GET, POST, PUT, DELETE, etc.)
-			// The second argument is the endpoint URL
-			xhr.open('POST', endpoint + '?' + params);
-			xhr.send();
-
+			}).catch(function (error) {
+				showStatus(messages.failed);
+			}).finally(function () {
+				form.removeAttribute('data-submitting');
+			});
 		};
 
-		// Submit the form
+		/**
+		 * Submit the form to the API
+		 */
 		var submitForm = function () {
 
 			// Add submitting state
 			form.setAttribute('data-submitting', true);
 
 			// Send the data to the MailChimp API
-			sendData(serialize(form));
+			sendData(serializeForm(form));
 
 		};
 
+		/**
+		 * Validate the email address
+		 * @return {Boolean} If true, email is valid
+		 */
 		var isEmail = function () {
 			return /^([^\x00-\x20\x22\x28\x29\x2c\x2e\x3a-\x3c\x3e\x40\x5b-\x5d\x7f-\xff]+|\x22([^\x0d\x22\x5c\x80-\xff]|\x5c[\x00-\x7f])*\x22)(\x2e([^\x00-\x20\x22\x28\x29\x2c\x2e\x3a-\x3c\x3e\x40\x5b-\x5d\x7f-\xff]+|\x22([^\x0d\x22\x5c\x80-\xff]|\x5c[\x00-\x7f])*\x22))*\x40([^\x00-\x20\x22\x28\x29\x2c\x2e\x3a-\x3c\x3e\x40\x5b-\x5d\x7f-\xff]+|\x5b([^\x0d\x5b-\x5d\x80-\xff]|\x5c[\x00-\x7f])*\x5d)(\x2e([^\x00-\x20\x22\x28\x29\x2c\x2e\x3a-\x3c\x3e\x40\x5b-\x5d\x7f-\xff]+|\x5b([^\x0d\x5b-\x5d\x80-\xff]|\x5c[\x00-\x7f])*\x5d))*(\.\w{2,})+$/.test(email.value);
 		};
 
+		/**
+		 * Validate the form fields
+		 * @return {Boolean} If true, form is valid
+		 */
 		var validate = function () {
 
 			// If no email is provided
@@ -202,6 +174,10 @@
 
 		};
 
+		/**
+		 * Handle submit events
+		 * @param  {Event} event The event object
+		 */
 		var submitHandler = function (event) {
 
 			// Stop form from submitting
@@ -210,8 +186,8 @@
 			// Don't run again if form currrent submitting
 			if (form.hasAttribute('data-submitting')) return;
 
-			// Clear the status
-			clearStatus();
+			// Show submitting status
+			showStatus(btn.getAttribute('data-processing'), true);
 
 			// Validate email
 			var valid = validate();
@@ -227,7 +203,6 @@
 		// Event Listeners & Inits
 		//
 
-		addRole();
 		form.addEventListener('submit', submitHandler, false);
 
 	};
